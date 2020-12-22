70f10070b93a312782dc8afc9af44b7f507e33f8
UsableSecGuy <paul@mbusecurity.com>
2020-12-22 17:07:46 -0600

    created: migration file for heroku

---
+++ migrations/versions/b0edba07f564_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: b0edba07f564
+Revises: 
+Create Date: 2020-12-22 17:02:08.035799
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b0edba07f564'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
